ac266c9758796b8971256657df81219dabd1cd4e
piotr.m <pmackowka@gmail.com>
2024-04-11 16:36:13 +0200

    98. Instrukcje Jinja: dla pętli i ustawiania zmiennych

--- lessons/models/marts/orders.sql
+++ lessons/models/marts/orders.sql
@@ -7,7 +7,13 @@ order_item_measures AS (
 		SUM(item_sale_price) AS total_sale_price,
 		SUM(product_cost) AS total_product_cost,
 		SUM(item_profit) AS total_profit,
-		SUM(item_discount) AS total_discount
+		SUM(item_discount) AS total_discount,
+
+		{# This is overkill, but a nice way to show how loops work with dbt Jinja templating #}
+		{%- set departments = ['Men', 'Women'] -%}
+		{%- for department_name in departments %}
+		SUM(IF(product_department = '{{department_name}}', item_sale_price, 0)) AS total_sold_{{department_name.lower()}}swear{% if not loop.last %},{% endif -%}
+		{% endfor %}
 
 	FROM {{ ref('int_ecommerce__order_items_products') }}
 	GROUP BY 1
@@ -26,6 +32,12 @@ SELECT
 	om.total_product_cost,
 	om.total_profit,
 	om.total_discount,
+
+	-- Columns from our templated Jinja statement
+	-- We could just hard code these if we wanted, e.g.: total_sold_menswear, total_sold_womenswear
+	{%- for department_name in departments %}
+	om.total_sold_{{department_name.lower()}}swear,
+	{%- endfor %}
 
 	-- In practise we'd calculate this column in the model itself, but it's
 	-- a good way to demonstrate how to use an ephemeral materialisation
